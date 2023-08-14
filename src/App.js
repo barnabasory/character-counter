004758df9c4385690a19430df9f5a1a38743cee4
barnabas2022 <123511857+barnabas2022@users.noreply.github.com>
2023-08-14 18:30:32 +0100

    change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Header, Dropdown, CharacterCounter } from "./exports/header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useToggleMenuContext } from "./context/MenuContext";
 function App() {
   const { toggleMenu } = useToggleMenuContext();
